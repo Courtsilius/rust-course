2dacb960fbe03cd7d8faca3df7433bc5a2d65c50
Pascal <18620961+Courtsilius@users.noreply.github.com>
2023-04-15 20:14:53 +0200

    boo

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,54 @@
-use std::env;
 use std::str::FromStr;
 
-fn main() {
-    let s = "1";
+fn boo() -> i32 {
+    std::env::args()
+        .nth(1)
+        .map(|s: String| {
+            i32::from_str(&s)
+        })
+        .unwrap_or(Ok(0))
+        .unwrap_or_else(|err| {
+            eprintln!("Error while parsing: {}", err);
+            0
+        })
+}
 
-    let a = i32::from_str(s);
-    let kek = env::args();
-    for argument in kek {
-        println!("{argument}");
+fn main() {
+    let x = boo();
+    println!("{x}");
+
+    //let mut kek = std::env::args();
+    //let first = kek.nth(1).unwrap_or_else(|| {
+    //    String::from("0")
+    //});
+    // println!("{first}");
+
+    /*
+
+
+    let f = std::env::args()
+        .nth(1)
+        .map(|s: String| {
+            i32::from_str(&s)
+        })
+        .unwrap_or(Ok(0))
+        .unwrap_or_else(|err| {
+            eprintln!("Error while parsing: {}", err);
+            0
+        });
+
+    println!("{f}");
+
+     */
+
+    /*
+    //list
+    let mut list = Vec::new();
+    list.push(1);
+
+    for elem in list.iter() {
+        println!("{elem}");
     }
+    */
 }
 
