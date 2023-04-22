836eb545906db33861ae72e2359960f886338f3b
Pascal <18620961+Courtsilius@users.noreply.github.com>
2023-04-22 16:37:52 +0200

    cargo format

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use std::f32;
-use std::str::FromStr;
 use std::io;
-use std::io::Write;
 use std::io::Stdin;
+use std::io::Write;
+use std::str::FromStr;
 
 // BMI calculator
 fn main() {
     let mut stdin = std::io::stdin();
 
     println!("Bitte Gewicht eingeben (in kg): ");
-    let weight:f64 = get_f64_from_input(&mut stdin);
+    let weight: f64 = get_f64_from_input(&mut stdin);
 
     println!("Bitte Größe eingeben (in cm): ");
     let height = get_f64_from_input(&mut stdin) / 100.0;
@@ -17,16 +17,16 @@ fn main() {
     // kg / m^2 = BMI
     let bmi = bmi(height, weight);
 
-    println!("Dein BMI: {}",bmi);
+    println!("Dein BMI: {}", bmi);
 }
 
-fn get_f64_from_input(stdin:&Stdin) -> f64 {
+fn get_f64_from_input(stdin: &Stdin) -> f64 {
     let mut buffer_height = String::new();
     stdin.read_line(&mut buffer_height);
     f64::from_str(buffer_height.trim()).unwrap()
 }
 
 // calculates bmi based on height and weight
-fn bmi(height:f64, weight:f64) -> f64 {
+fn bmi(height: f64, weight: f64) -> f64 {
     weight / (f64::powf(height, 2.0))
 }
